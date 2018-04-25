7b301baabb6c214e796106652b2a999db4e0fbcb
Audrey <anfletcher24@gmail.com>
2018-04-25 16:15:57 -0500

    intital commit

---
+++ server.js
@@ -0,0 +1,22 @@
+
+
+// var express = require('express');
+// var app = express();
+// var path = require("path");
+// var PORT = process.env.PORT || 3000;
+// var bodyParser = require("body-parser");
+
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// app.use(express.static(path.join(__dirname, 'public/index.html')));
+
+
+
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "public"));
+// });
+
+// app.listen(3000, () => console.log('Example app listening on ' + PORT + '!'));
